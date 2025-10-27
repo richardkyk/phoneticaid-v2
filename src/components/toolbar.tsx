814c563d110b987a969d228ce9efe1a1b3d0088d
richardkyk <28803078+richardkyk@users.noreply.github.com>
2025-10-27 17:07:15 +1100

    refactor: update toolbar

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,157 +1,301 @@
 import { useDocumentStore } from '@/lib/stores/document-store'
-import { Slider } from './ui/slider'
 import { Switch } from './ui/switch'
 import { Button } from './ui/button'
-import { RedoIcon, UndoIcon } from 'lucide-react'
+import {
+  BugIcon,
+  ChevronDown,
+  LayoutIcon,
+  Maximize2Icon,
+  MinusIcon,
+  MoveIcon,
+  PlusIcon,
+  RedoIcon,
+  Type,
+  UndoIcon,
+} from 'lucide-react'
 import { useHistoryStore } from '@/lib/stores/history-store'
 import { ClientOnly } from '@tanstack/react-router'
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
+import { ButtonGroup } from './ui/button-group'
+import { Separator } from './ui/separator'
+import { cn } from '@/lib/utils'
 
 export default function Toolbar() {
+  return (
+    <ClientOnly>
+      <div className="p-2 flex gap-4 h-full font-sans items-center">
+        <EditPopover />
+        <Separator
+          orientation="vertical"
+          className="data-[orientation=vertical]:h-[20px]"
+        />
+        <LayoutPopover />
+        <MarginPopover />
+        <GapPopover />
+        <TextPopover />
+        <DebugButton className="ml-auto" />
+      </div>
+    </ClientOnly>
+  )
+}
+
+interface NumberControlProps {
+  label: string
+  value: number
+  setValue: (value: number) => void
+  min: number
+  max: number
+  step: number
+  decimalPoints?: number
+}
+function NumberControl({
+  label,
+  value,
+  setValue,
+  min,
+  max,
+  step,
+  decimalPoints = 0,
+}: NumberControlProps) {
+  const clamp = (n: number) => Math.max(min, Math.min(max, n))
+
+  return (
+    <div className="flex items-center justify-between">
+      <label className="text-xs text-gray-600">{label}</label>
+      <ButtonGroup>
+        <Button
+          className="shadow-none"
+          variant="outline"
+          size="icon-sm"
+          onClick={() => setValue(clamp(value - step))}
+          disabled={value <= min}
+        >
+          <MinusIcon />
+        </Button>
+
+        <input
+          type="number"
+          value={value.toFixed(decimalPoints)}
+          onChange={(e) => setValue(clamp(Number(e.target.value)))}
+          className="border text-center w-12 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+        />
+
+        <Button
+          className="shadow-none"
+          variant="outline"
+          size="icon-sm"
+          onClick={() => setValue(clamp(value + step))}
+          disabled={value >= max}
+        >
+          <PlusIcon />
+        </Button>
+      </ButtonGroup>
+    </div>
+  )
+}
+
+interface DebugProps {
+  className?: string
+}
+function DebugButton(props: DebugProps) {
+  const { debug, toggleDebug } = useDocumentStore()
+  return (
+    <Button
+      className={cn(props.className)}
+      onClick={() => toggleDebug()}
+      variant="ghost"
+    >
+      <BugIcon
+        className={cn('size-4', debug ? 'text-primary' : 'text-gray-400')}
+      />
+    </Button>
+  )
+}
+
+function EditPopover() {
+  const { past, future } = useHistoryStore()
+
+  return (
+    <div className="flex items-center">
+      <Button
+        onClick={() => useHistoryStore.getState().undo()}
+        size="icon-sm"
+        variant="ghost"
+        disabled={past.length === 0}
+      >
+        <UndoIcon />
+      </Button>
+      <Button
+        onClick={() => useHistoryStore.getState().redo()}
+        size="icon-sm"
+        variant="ghost"
+        disabled={future.length === 0}
+      >
+        <RedoIcon />
+      </Button>
+    </div>
+  )
+}
+
+function TextPopover() {
   const {
     fontSize,
     pinyinSize,
     pinyinOffset,
     pinyinPosition,
-    columns,
-    gapX,
-    gapY,
-    marginX,
-    marginY,
-    debug,
     setFontSize,
     setPinyinSize,
     setPinyinOffset,
     setPinyinPosition,
-    setColumns,
-    setGapX,
-    setGapY,
-    setMarginX,
-    setMarginY,
-    toggleDebug,
   } = useDocumentStore()
-
-  const { past, future } = useHistoryStore()
-
   return (
-    <ClientOnly>
-      <div className="p-2 flex gap-4">
-        <div>
-          Font size:
-          <Slider
-            defaultValue={[fontSize]}
-            onValueChange={([value]) => setFontSize(value)}
-            max={50}
-            step={1}
-          />
+    <Popover>
+      <PopoverTrigger>
+        <div className="flex items-center gap-1">
+          <Type className="size-4" />
+          <ChevronDown className="size-4" />
         </div>
-        <div>
-          Pinyin size:
-          <Slider
-            defaultValue={[pinyinSize]}
-            onValueChange={([value]) => setPinyinSize(value)}
-            max={20}
-            step={0.1}
-          />
-        </div>
-        <div>
-          Pinyin offset:
-          <Slider
-            defaultValue={[pinyinOffset]}
-            onValueChange={([value]) => setPinyinOffset(value)}
-            max={20}
-            step={0.1}
-          />
-        </div>
-        <div>
-          Pinyin position (Above):
-          <Switch
-            className="block"
-            checked={pinyinPosition === 'top'}
-            onCheckedChange={() =>
-              setPinyinPosition(pinyinPosition === 'top' ? 'bottom' : 'top')
-            }
-          />
-        </div>
-        <div>
-          Columns:
-          <Slider
-            defaultValue={[columns]}
-            onValueChange={([value]) => setColumns(value)}
-            max={24}
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="space-y-3 font-sans">
+          <NumberControl
+            label="Font Size"
+            value={fontSize}
+            setValue={setFontSize}
             min={1}
+            max={100}
+            step={1}
+          />
+          <Separator />
+          <NumberControl
+            label="Pinyin Size"
+            value={pinyinSize}
+            setValue={setPinyinSize}
+            min={0}
+            max={20}
+            step={0.1}
+            decimalPoints={1}
+          />
+          <NumberControl
+            label="Pinyin Offset"
+            value={pinyinOffset}
+            setValue={setPinyinOffset}
+            min={0}
+            max={20}
+            step={0.1}
+            decimalPoints={1}
+          />
+
+          <div className="flex items-center justify-between h-8">
+            <label className="text-xs text-gray-600">
+              Pinyin Position (Above)
+            </label>
+            <Switch
+              className="block"
+              checked={pinyinPosition === 'top'}
+              onCheckedChange={() =>
+                setPinyinPosition(pinyinPosition === 'top' ? 'bottom' : 'top')
+              }
+            />
+          </div>
+        </div>
+      </PopoverContent>
+    </Popover>
+  )
+}
+
+function LayoutPopover() {
+  const { columns, setColumns } = useDocumentStore()
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <div className="flex items-center gap-1">
+          <LayoutIcon className="size-4" />
+          <ChevronDown className="size-4" />
+        </div>
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="space-y-3 font-sans">
+          <NumberControl
+            label="Columns"
+            value={columns}
+            setValue={setColumns}
+            min={1}
+            max={30}
             step={1}
           />
         </div>
-        <div>
-          Gap X:
-          <Slider
-            defaultValue={[gapX]}
-            onValueChange={([value]) => setGapX(value)}
-            max={30}
-            step={0.1}
-          />
-        </div>
-        <div>
-          Gap Y:
-          <Slider
-            defaultValue={[gapY]}
-            onValueChange={([value]) => setGapY(value)}
-            max={30}
-            step={0.1}
-          />
-        </div>
+      </PopoverContent>
+    </Popover>
+  )
+}
 
-        <div>
-          Margin X:
-          <Slider
-            defaultValue={[marginX]}
-            onValueChange={([value]) => setMarginX(value)}
+function GapPopover() {
+  const { gapX, gapY, setGapX, setGapY } = useDocumentStore()
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <div className="flex items-center gap-1">
+          <MoveIcon className="size-4" />
+          <ChevronDown className="size-4" />
+        </div>
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="space-y-3 font-sans">
+          <NumberControl
+            label="Gap X"
+            value={gapX}
+            setValue={setGapX}
+            min={0}
+            max={30}
+            step={0.1}
+            decimalPoints={1}
+          />
+          <NumberControl
+            label="Gap Y"
+            value={gapY}
+            setValue={setGapY}
+            min={0}
+            max={30}
+            step={0.1}
+            decimalPoints={1}
+          />
+        </div>
+      </PopoverContent>
+    </Popover>
+  )
+}
+
+function MarginPopover() {
+  const { marginX, marginY, setMarginX, setMarginY } = useDocumentStore()
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <div className="flex items-center gap-1">
+          <Maximize2Icon className="size-4" />
+          <ChevronDown className="size-4" />
+        </div>
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="space-y-3 font-sans">
+          <NumberControl
+            label="Margin X"
+            value={marginX}
+            setValue={setMarginX}
+            min={0}
+            max={100}
+            step={5}
+          />
+          <NumberControl
+            label="Margin Y"
+            value={marginY}
+            setValue={setMarginY}
+            min={0}
             max={100}
             step={5}
           />
         </div>
-        <div>
-          Margin Y:
-          <Slider
-            defaultValue={[marginY]}
-            onValueChange={([value]) => setMarginY(value)}
-            max={100}
-            step={5}
-          />
-        </div>
-
-        <div>
-          Undo:
-          <Button
-            onClick={() => useHistoryStore.getState().undo()}
-            className="block h-8"
-            variant="outline"
-            disabled={past.length === 0}
-          >
-            <UndoIcon />
-          </Button>
-        </div>
-        <div>
-          Redo:
-          <Button
-            onClick={() => useHistoryStore.getState().redo()}
-            className="block h-8"
-            variant="outline"
-            disabled={future.length === 0}
-          >
-            <RedoIcon />
-          </Button>
-        </div>
-
-        <div className="ml-auto">
-          Debug:
-          <Switch
-            className="block"
-            checked={debug}
-            onCheckedChange={toggleDebug}
-          />
-        </div>
-      </div>
-    </ClientOnly>
+      </PopoverContent>
+    </Popover>
   )
 }
