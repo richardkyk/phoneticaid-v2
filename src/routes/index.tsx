99820dc26617d854da6f4aade802673f5d9d30bd
richardkyk <28803078+richardkyk@users.noreply.github.com>
2025-11-06 13:51:26 +1100

    refactor: add sonner

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { Document } from '@/components/document'
 import { Translator } from '@/components/translator'
+import { Toaster } from '@/components/ui/sonner'
 import { createFileRoute } from '@tanstack/react-router'
 import { Fragment } from 'react/jsx-runtime'
 
@@ -13,6 +14,7 @@ function App() {
     <Fragment>
       <Translator />
       <Document />
+      <Toaster position="top-right" />
     </Fragment>
   )
 }
